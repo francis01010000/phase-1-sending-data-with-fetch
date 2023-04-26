ec751b27fe506845f74beae929eb8d48e98e38b2
Francisco Perez <Franciscop693@gmail.com>
2023-04-26 02:05:02 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+function submitData(name, email){
+    const userData = {
+        name: name,
+        email: email,
+      };
+      const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify(userData),
+      };
+      
+      return fetch("http://localhost:3000/users", configurationObject)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (object) {
+            let p = document.createElement("p");
+            p.innerHTML = object.id;
+            document.body.appendChild(p);
+            console.log(object);
+        })
+        .catch(function (error) {
+            let h1 = document.createElement("h1");
+            h1.innerHTML = error.message;
+            document.body.appendChild(h1)
+            console.log(error.message)
+        });
+}
+submitData()
